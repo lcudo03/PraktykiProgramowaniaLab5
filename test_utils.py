8e61dc011e81b0371dae0c2ad30cdb6f93dc67c5
Lukasz <cudo447@gmail.com>
2026-05-28 17:40:40 +0200

    Code clarity fixes

--- test_utils.py
+++ test_utils.py
@@ -8,13 +8,17 @@ def test_add(a, b, expected):
     assert result == expected
 
 
-@pytest.mark.parametrize("a, b, expected", [(1, 2, -1), (2, 3, -1), (3, 4, -1), (4, 5, -1)])
+@pytest.mark.parametrize(
+    "a, b, expected", [(1, 2, -1), (2, 3, -1), (3, 4, -1), (4, 5, -1)]
+)
 def test_subtract(a, b, expected):
     result = utils.subtract(a, b)
     assert result == expected
 
 
-@pytest.mark.parametrize("a, b, expected", [(1, 2, 2), (2, 3, 6), (3, 4, 12), (4, 5, 20)])
+@pytest.mark.parametrize(
+    "a, b, expected", [(1, 2, 2), (2, 3, 6), (3, 4, 12), (4, 5, 20)]
+)
 def test_multiply(a, b, expected):
     result = utils.multiply(a, b)
     assert result == expected
